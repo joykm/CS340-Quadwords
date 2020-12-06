4788a0b81e93cc151ea9baec0476b1b277a42435
Kevin Joy <joykm88@gmail.com>
2020-12-05 22:16:59 -0600

    fixed auto populate for dropdown and date

--- public/scripts/update_project.js
+++ public/scripts/update_project.js
@@ -18,6 +18,22 @@ for (var row of table_rows) {
             var priority = row.querySelector('.table-priority-id').innerHTML
             var startDate = row.querySelector('.table-start-date').innerHTML
             var endDate = row.querySelector('.table-end-date').innerHTML
+
+            // Clean up text inputs.
+            status = status.replace("&nbsp;", "")
+            priority = priority.replace("&nbsp;", "")
+            
+            // Remove all whitespace from string.
+            status = status.replace(/\s/g,'')
+            priority = priority.replace(/\s/g,'')
+            startDate = startDate.replace(/\s/g,'')
+            endDate = endDate.replace(/\s/g,'')
+
+            // Reverse date format for html form.
+            startDate = startDate.split('-')
+            startDate = startDate[2] + '-' + startDate[0] + '-' + startDate[1]
+            endDate = endDate.split('-')
+            endDate = endDate[2] + '-' + endDate[0] + '-' + endDate[1]
             
             // Get access to the input feilds in the form.
             var modalProjectID = document.querySelector('#modal_update_projectID')
@@ -25,17 +41,37 @@ for (var row of table_rows) {
             var modalInputDescription = document.querySelector('#modal_update_project_description')
             var modalInputStatus = document.querySelector('#modal_update_project_status')
             var modalInputPriority = document.querySelector('#modal_update_project_priority')
-            var modalInputStartDate = document.querySelector('#modal_update_project_start_date')
-            var modalInputEndDate = document.querySelector('#modal_update_project_end_date')
 
-            // Populate the input fields with the previous data.
+            // Populate text input fields with the previous data.
             modalProjectID.value = projectID
             modalInputName.value = name
             modalInputDescription.value = description
-            modalInputStatus.value = status
-            modalInputPriority.value = priority
-            modalInputStartDate.value = startDate
-            modalInputEndDate.value = endDate
+
+            // Populate date input fields with previousu data.
+            document.getElementById('modal_update_project_start_date').value = startDate
+            document.getElementById('modal_update_project_end_date').value = endDate
+
+            // Populate dropdown menus with previous data.
+            for(var i, j = 0; i = modalInputStatus.options[j]; j++) {
+                cleanTxt = i.value
+
+                // Remove all whitespace from string
+                cleanTxt = cleanTxt.replace(/\s/g,'')
+                if(cleanTxt == status) {
+                    modalInputStatus.selectedIndex = j;
+                    break;
+                }
+            }
+            for(var i, j = 0; i = modalInputPriority.options[j]; j++) {
+                cleanTxt = i.value
+
+                // Remove all whitespace from string
+                cleanTxt = cleanTxt.replace(/\s/g,'')
+                if(cleanTxt == priority) {
+                    modalInputPriority.selectedIndex = j;
+                    break;
+                }
+            }
 
             // Bootstrap code to make the model appear.
             $("#updateProjectModal").modal("show")
@@ -49,9 +85,23 @@ for (var row of table_rows) {
 }
 
 // Event listener on modal form submission button
-var form = document.getElementsByClassName('update-form')[0]
-form.addEventListener('submit', function(event) {
+document.getElementById("update-project-form").addEventListener("submit", function(e){
+    e.preventDefault()
+
+    // Get access to the input feilds in the form.
+    var modalInputStatus = document.querySelector('#modal_update_project_status')
+    var modalInputPriority = document.querySelector('#modal_update_project_priority')
+
+    // Ensure null values are submited for foriegn key fields, as opposed to empty strings.
+    if (modalInputStatus.value == "") {
+        modalInputStatus.value = null 
+    }
+    if (modalInputPriority.value == "") {
+        modalInputPriority.value = null 
+    }
+
     // Update the data here when the database is created
+    document.getElementById('update-project-form').submit(); 
 })
 
 // Event listener on delete button within modal form
